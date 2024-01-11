02613b82a5657d524662b9f2269652d0511b5668
nikita <neketa.erma13@gmail.com>
2024-01-11 12:38:55 +0300

    feat(core): added new font-family font='akony' or font='onest'

---
+++ src/shared/ui/button-event/button-event.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonEventComponent } from './button-event.component';
+
+describe('ButtonEventComponent', () => {
+  let component: ButtonEventComponent;
+  let fixture: ComponentFixture<ButtonEventComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ButtonEventComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ButtonEventComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
