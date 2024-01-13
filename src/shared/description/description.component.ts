72a23c7735d9f5add0c7979d8d3f0761a6181c48
nikita <neketa.erma13@gmail.com>
2024-01-13 21:03:17 +0300

    feat(component): 1)added shared description

---
+++ src/shared/description/description.component.ts
@@ -0,0 +1,14 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-description',
+  standalone: true,
+  imports: [],
+  templateUrl: './description.component.html',
+  styleUrl: './description.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
+})
+export class DescriptionComponent {
+
+}
