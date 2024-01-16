5dd9ea785549507f36364b658ac984b88f9ff8bb
nikita <neketa.erma13@gmail.com>
2024-01-16 14:55:25 +0300

    feat(directive) added standalone directive cdFlashing which monitors the mechanism for detecting changes

---
+++ src/shared/directives/cdflashing-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CDFlashingDirectiveDirective } from './cdflashing-directive.directive';
+
+describe('CDFlashingDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CDFlashingDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
