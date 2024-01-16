68588ac09f941338f9aa67479426e689fcc898e0
nikita <neketa.erma13@gmail.com>
2024-01-16 23:12:59 +0300

    chore(core) deleted no actual unit test

--- src/shared/directives/cdflashing-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CDFlashingDirectiveDirective } from './cdflashing-directive.directive';
-
-describe('CDFlashingDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CDFlashingDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
