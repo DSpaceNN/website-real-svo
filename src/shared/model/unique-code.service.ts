53bef1091bd79ce4cc0b55dd907e41f5516d3695
nikita <neketa.erma13@gmail.com>
2024-01-20 22:51:13 +0300

    refactor(service) added readonly parametr

--- src/shared/model/unique-code.service.ts
+++ src/shared/model/unique-code.service.ts
@@ -5,7 +5,7 @@ import {computed, Injectable, signal} from '@angular/core';
 })
 export class UniqueCodeService {
  #numberCart = signal<number>(32)
-  public numberCart = computed(this.#numberCart)
+  public readonly numberCart = computed(this.#numberCart)
   constructor() { }
 set (value:number) {
    this.#numberCart.set(value)
