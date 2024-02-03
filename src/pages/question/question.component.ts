79b5d129b99c07721570add67c96c6ae50529c21
nikita <neketa.erma13@gmail.com>
2024-02-03 12:49:44 +0300

    fix(questions/page): fixed scroll height container

--- src/pages/question/question.component.ts
+++ src/pages/question/question.component.ts
@@ -26,7 +26,7 @@ import {LineLightGrayComponent} from "../../shared/ui/line-light-gray/line-light
     LineLightGrayComponent
   ],
   template: `
-    <section class="">
+    <section >
       <app-sub-header></app-sub-header>
       <div class="mb-4">
         <div class="flex  gap-2 mb-4 justify-start  items-start md:items-center">
@@ -37,6 +37,7 @@ import {LineLightGrayComponent} from "../../shared/ui/line-light-gray/line-light
       </div>
         <app-line-white class="w-full"></app-line-white>
       </div>
+      <div class="h-scroll-y overflow-y-scroll  no-scrollbar">
             <app-question-answer-option   [answers]="answers">
               <ng-template #questionAnswerOption let-answer>
                   <app-radio-button (valueChange)="onChange($event)" [radioButtonValue]="answer.value">
@@ -51,7 +52,7 @@ import {LineLightGrayComponent} from "../../shared/ui/line-light-gray/line-light
                 <app-line-light-gray class="block my-3"></app-line-light-gray>
               </ng-template>
             </app-question-answer-option>
-
+      </div>
 <!--      absolute block-->
       <app-cta-card-wrapper class="fixed w-full left-0 right-0 bottom-0">
         <ng-template #ctaCard>
@@ -70,6 +71,10 @@ import {LineLightGrayComponent} from "../../shared/ui/line-light-gray/line-light
     width: 138px;
     height: 38px;
   }
+  .h-scroll-y {
+    height: calc(100vh - 310px);
+
+  }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -79,7 +84,7 @@ export default class QuestionComponent {
     console.log(event)
     this.selectedAnswer.set(event)
   }
-  answers =   [{id:1, value:'hello124', text:'helloBrot1'},{id:2, value:'hell4o4', text:'helloBrot'},{id:3, value:'he2ll4o', text:'helloBrot3'},{id:12, value:'h4ell4o', text:'helloBrot'},{id:62, value:'hellf14o', text:'helloBrot'},{id:555, value:'hellf4o', text:'helloBrot'},{id:124, value:'hellas4o', text:'helloBrot'},]
+  answers =   [{id:1, value:'hello124', text:'helloBrot1'},{id:2, value:'hell4o4', text:'helloBrot'},{id:3, value:'he2ll4o', text:'helloBrot3'},{id:12, value:'h4ell4o', text:'helloBrot'},{id:62, value:'hellf14o', text:'helloBrot'},{id:555, value:'hellf4o', text:'helloBrot'},{id:124, value:'hellas4o', text:'helloBrot'},{id:124, value:'testlast', text:'helloBrot'}]
 click1() {
   console.log(1)
 }
