06cdbd025e08b926610e2ea389cd6ec96b700079
nikita <neketa.erma13@gmail.com>
2024-02-11 16:02:33 +0300

    feat(core): added type to collection api-urls

--- src/shared/model/types/variables.ts
+++ src/shared/model/types/variables.ts
@@ -5,3 +5,4 @@ export enum RequestMethods {
   PUT = 'PUT',
 }
 export type APIEndpoint = { url: string; method: RequestMethods };
+export interface APICollection  { [key:string]: APIEndpoint }
--- src/shared/model/utils/api.endpoints.ts
+++ src/shared/model/utils/api.endpoints.ts
@@ -1,6 +1,6 @@
-import {RequestMethods} from "../types/variables";
+import {APICollection, RequestMethods} from "../types/variables";
 
-export const API = {
+export const API:APICollection = {
   CREATE_SURVEY: { url: '/Surveys/CreateOrEditSurvey', method: RequestMethods.POST },
   GET_SURVEY: {url: '/Surveys/GetSurveys', method: RequestMethods.GET}
 };
