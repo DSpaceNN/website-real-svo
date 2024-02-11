9a00b4a06ce3fb9c2537e40922c24dac56b68387
nikita <neketa.erma13@gmail.com>
2024-02-11 16:07:23 +0300

    chore(api): deleted no actual type

--- src/shared/model/utils/api.endpoints.ts
+++ src/shared/model/utils/api.endpoints.ts
@@ -1,6 +1,6 @@
 import {APICollection, RequestMethods} from "../types/variables";
 
-export const API:APICollection = {
+export const API = {
   CREATE_SURVEY: { url: '/Surveys/CreateOrEditSurvey', method: RequestMethods.POST },
   GET_SURVEY: {url: '/Surveys/GetSurveys', method: RequestMethods.GET}
 };
